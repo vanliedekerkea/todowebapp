9387e1b7d8d268578a8c512bc19fe46612387542
Arno Van Liedekerke <arno.vanliedekerke@gmail.com>
2018-03-25 12:59:22 +0200

    User can view a todo item and switch between view and edit mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core'; 
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  editMode = false;
+
+  todo: Todo = {
+    title: 'Fare pagamenti in banca',
+    description: 'Andare in banca per pagare la luce, il gas e l\'affitto'
+  };
+
+}
