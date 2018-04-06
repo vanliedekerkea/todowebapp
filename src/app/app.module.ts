5cb4eb4135eaea1fa3d9b464bd177a3c9cdeaf87
Arno Van Liedekerke <arno.vanliedekerke@gmail.com>
2018-04-06 10:37:19 +0200

    fixed error message on browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     MatButtonModule,
     MatCheckboxModule,
